35d634fae53c2e9bf2c9b9569c282f40ffa4f0a6
ManuelFLopez <lopez.inigo.manuel.francisco@iescamas.es>
2026-02-23 13:51:34 +0100

    Completado programa cuadrado y punto

--- code/src/main/java/com/endes/figuras/Cuadrado.java
+++ code/src/main/java/com/endes/figuras/Cuadrado.java
@@ -1,22 +1,40 @@
 package com.endes.figuras;
 
 public class Cuadrado extends FiguraGeometrica {
+	private Punto esquina;
+    private double lado;
 
-	public Cuadrado(String nombre, String color) {
+	public Cuadrado(String nombre, String color, Punto esquina, double lado) {
 		super(nombre, color);
-		// TODO Auto-generated constructor stub
+		this.esquina = esquina;
+        this.lado = lado;
 	}
 
 	@Override
 	public double area() {
-		// TODO Auto-generated method stub
-		return 0;
+		return lado * lado;
 	}
 
 	@Override
 	public double perimetro() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 4 * lado;
 	}
+	
+	public void rotar(double angulo) {
+		System.out.println("Rotando el cuadrado " + angulo + " grados.");
+	}
+	
+	public void trasladar(double dx, double dy) {
+		esquina.setX(esquina.getX() + dx);
+        esquina.setY(esquina.getY() + dy);
+	}
+	
+	 public Punto getEsquina() {
+	        return esquina;
+	    }
 
+	    public double getLado() {
+	        return lado;
+
+	    }
 }
