c3b4b379ca390737c1a674b688e04ae2e55b082a
ManuelFLopez <lopez.inigo.manuel.francisco@iescamas.es>
2026-03-09 12:56:46 +0100

    Cambios Guardados

--- code/src/main/java/com/endes/figuras/Punto.java
+++ code/src/main/java/com/endes/figuras/Punto.java
@@ -1,5 +1,35 @@
 package com.endes.figuras;
 
 public class Punto {
+	
+	private double x;
+	private double y;	
+	
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
 
+	public void setX(double x) {
+		this.x = x;
+		
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+		
+	}
+	
+	public double distancia(Punto otro) {
+		double distancia = Math.sqrt(Math.pow(otro.getX() - this.getX(),2) + Math.pow(otro.getY()- this.getY(), 2));
+		return distancia;
+	}
 }
---
+++ code/src/test/java/com/endes/figuras/TrianguloTest.java
@@ -0,0 +1,32 @@
+package com.endes.figuras;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class TrianguloTest {
+	
+	private Triangulo triangulo;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		//String nombre, String color, vertice1, vertice2, vertice3
+		Punto v1 = new Punto(0,1);
+		Punto v2 = new Punto(0,0);
+		Punto v3 = new Punto(2,0);
+				
+		triangulo = new Triangulo("trieangulo", "negro", v1, v2, v3);
+	}
+
+	@Disabled
+	@Test
+	void testConstructor() {
+		Punto v1_esperado = new Punto(0,1);
+		Punto v2_esperado = new Punto(0,0);
+		Punto v3_esperado = new Punto(2,0);
+	
+		assertEquals(v1_esperado, triangulo.getVertice1());
+	}
+
+}
